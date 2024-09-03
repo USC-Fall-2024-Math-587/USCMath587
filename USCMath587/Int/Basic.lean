2cf52e1eb58fe2503c642e26477871665fdabaea
Matthew Ballard <matt@mrb.email>
2024-09-03 15:24:34 -0400

    add

---
+++ USCMath587/Int/Basic.lean
@@ -0,0 +1,11 @@
+import Batteries.Data.Int.Lemmas
+
+open Int
+
+theorem natAbs_emod_lt_natAbs (m : Int) {n : Int} (hn : n ≠ 0) :
+    (m % n).natAbs < n.natAbs := by
+  obtain ⟨n, hn | hn⟩ := eq_nat_or_neg n
+  · rw [hn]
+    exact natAbs_lt_natAbs_of_nonneg_of_lt (emod_nonneg _ (by omega)) (emod_lt_of_pos _ (by omega))
+  · rw [hn, emod_neg, natAbs_neg]
+    exact natAbs_lt_natAbs_of_nonneg_of_lt (emod_nonneg _ (by omega)) (emod_lt_of_pos _ (by omega))
